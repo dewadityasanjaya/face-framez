25187b8f954bd7afe9980c5058363a3e55ee7f18
Dewa Aditya <dewadityasanjaya@gmail.com>
2023-05-06 06:20:39 +0700

    add particle background and Rank component

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import Logo from './components/Logo/Logo'
 import Rank from './components/Rank/Rank'
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm'
 import FaceRecognition from './components/FaceRecognition/FaceRecognition'
+import ParticlesBg from 'particles-bg'
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
       <div className='App'>
+        <ParticlesBg color='#FFFFFF' type="cobweb" bg={true} />
         <Navigation />
         <Logo />
         <Rank />
--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -3,7 +3,12 @@ import React from "react";
 const Rank = () => {
     return (
         <div>
-            Hello world!
+            <div className="f3 white">
+                {'Your current rank is ...'}
+            </div>
+            <div className="f1 white">
+                {'#2'}
+            </div>
         </div>
     )
 }
