f95ec1f9026699b44d35993277cefc4fad32e494
Dewa Aditya <dewadityasanjaya@gmail.com>
2023-05-07 20:23:02 +0700

    add Clarifai API and connect it with imageLinkForm

--- src/App.js
+++ src/App.js
@@ -7,8 +7,65 @@ import FaceRecognition from './components/FaceRecognition/FaceRecognition'
 import ParticlesBg from 'particles-bg'
 import './App.css';
 
+// Clarifai Setup
+const MODEL_ID = 'face-detection';
+
+const returnClarifaiRequestOption = (imageUrl) => {
+  const PAT = 'adc67d41e4ef4ff3a2abfbde9939de10';
+  const USER_ID = 'dewadityasanjaya';
+  const APP_ID = 'face-framez';
+  const IMAGE_URL = imageUrl;
+
+  const raw = JSON.stringify({
+    "user_app_id": {
+      "user_id": USER_ID,
+      "app_id": APP_ID
+    },
+    "inputs": [
+      {
+        "data": {
+          "image": {
+            "url": IMAGE_URL
+          }
+        }
+      }
+    ]
+  });
+
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Authorization': 'Key ' + PAT
+    },
+    body: raw
+  };
+
+  return requestOptions;
+}
 
 class App extends Component {
+  //App State
+  constructor() {
+    super();
+    this.state = {
+      imgUrlInput: ''
+    }
+  }
+
+  //Function to Change Input Value
+  onInputChange = (event) => {
+    this.setState({ imgUrlInput: event.target.value });
+  }
+
+  //Function to Submit Image URL to Clarifai
+  onSubmit = () => {
+    fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/outputs", returnClarifaiRequestOption(this.state.imgUrlInput))
+      .then(response => response.json())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+  }
+
   render() {
     return (
       <div className='App'>
@@ -16,12 +73,11 @@ class App extends Component {
         <Navigation />
         <Logo />
         <Rank />
-        <ImageLinkForm />
+        <ImageLinkForm onInputChange={this.onInputChange} onSubmit={this.onSubmit} />
         <FaceRecognition />
       </div>
     );
   }
-
 }
 
 export default App;
